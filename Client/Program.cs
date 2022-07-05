e13b62cc667b6305aa54c91c3812ff88f0858c3f
stefanom13 <stefanomancuso13@gmail.com>
2022-07-05 17:56:03 +0200

    Aggiungere i file di progetto.

---
+++ Client/Program.cs
@@ -0,0 +1,36 @@
+﻿//il cliente assegna delle attività allo svilppatore (dal suo file al file dello sviluppatore).
+
+//questa attività avviene casualmente in un intervallo di 5-10 secondi; ( Thread.Sleep(1000) );
+
+//qualce volta perà il cliente viene distratto (il programma si chiude casualmente? - oppure c'è un'eccezione)
+
+//quindi il cliente deve ricominciare possibilmente senza riassegnare task già assegnati allo sviluppatore ( che controlli ?? )
+
+
+
+//sviluppi
+
+
+//1.    creare i reader e i writer per leggere dal file
+//      C:\Users\mistre\source\repos\csharp-developer-simulator\client_activity.txt
+//      al file C:\Users\mistre\source\repos\csharp-developer-simulator\developer_activity.txt
+//      dovremmo fare una apertura in lettura di un file e in scrittura di un altro file
+
+//2. leggere la linea = attività e salvarala da qualche parte --> read su client_activity.txt
+//3. scrivere l'attivà che abbiamo recuperato nell'atro file --> write su developer_activity.txt
+
+//4. Tread.Sleep(...tot secondi casuali...)
+
+//5. si ricomincia da punto 2 fino alla fine del file client_activity.txt
+
+
+StreamReader clientActivity = File.OpenText("C:\\Users\\Utente\\source\\repos\\csharp-developer-simulator\\client_activity.txt");
+
+while (!clientActivity.EndOfStream)
+{
+    StreamWriter developerActivity = File.CreateText("C:\\Users\\Utente\\source\\repos\\csharp-developer-simulator\\developer_activity.txt");
+
+    string activity = clientActivity.ReadLine();
+    developerActivity.WriteLine(activity);
+
+}
---
+++ Developer/Program.cs
@@ -0,0 +1,34 @@
+﻿//lo sviluppatore legge le attività dal suo file di attività (developer_activity) da risolvere
+//per risolvere le attività ci impega dai 3 ai 15 secondi
+//se le risolve le elimina dalla lista
+//se non le risolve la porta in fondo alla lista per farle in un secondo momento
+
+
+//1. creare il reader 
+//2. aprire il file developer_activity.txt
+
+//3. leggo la riga = task ??? 
+
+//4. bool risolta = true || false --> new Random().NextDouble() > 0.5;
+
+//5. risolta == false --> dobbiamo scrivere il file di modo che l'ultima riga sia il task corrente.
+//5.1 risolta == true --> dobbiamo scrivere il file in modo che la lista sia aggiornata senza il task corrente.
+
+//6. ripeto dl punto ?? 
+
+
+StreamReader clientActivity = File.OpenText("C:\\Users\\Utente\\source\\repos\\csharp-developer-simulator\\developer_activity.txt");
+
+while (!clientActivity.EndOfStream)
+{
+    string activity = clientActivity.ReadLine();
+    Console.WriteLine("Presa in carico attivtà: {0}", activity);
+
+    int resolveTime = new Random().Next(3, 16);
+    Console.WriteLine("Risoluzione in corso...");
+    Thread.Sleep(resolveTime);
+
+    bool risolta = new Random().NextDouble() > 0.5;
+
+
+}
